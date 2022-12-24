4cfba6434d9fb175cae96cd8ae268a45de02c2b2
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2022-12-24 13:35:26 +0200

    "useState" hook overview

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### React Hooks Overview
 
 ***
+
+1. [Using the State Hook](https://reactjs.org/docs/hooks-state.html)
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientList from "./IngredientList";
 
-function Ingredients() {
-  return (
-    <div className="App">
-      <IngredientForm />
+const Ingredients = () => {
+    const [userIngredients, setUserIngredients] = useState([]);
 
-      <section>
-        <Search />
-        {/* Need to add list here! */}
-      </section>
-    </div>
-  );
+    const addIngredientHandler = ingredient => {
+        fetch('https://react-http-25001-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json', {
+                method: "POST",
+                body: JSON.stringify(ingredient),
+                headers: {'Content-Type': 'application/json'}
+            }
+        ).then(response => {
+            return response.json()
+        }).then(data => {
+            setUserIngredients(
+                (prevState) => [
+                    ...prevState,
+                    {id: data.name, ...ingredient}
+                ]
+            );
+        });
+    }
+    const removeIngredientHandler = ingredientId => {
+        setUserIngredients(
+            prevState => prevState.filter(ingredient => ingredient.id !== ingredientId)
+        );
+    }
+
+    return (
+        <div className="App">
+            <IngredientForm onAddIngredient={addIngredientHandler}/>
+
+            <section>
+                <Search/>
+                <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
+            </section>
+        </div>
+    );
 }
 
 export default Ingredients;
